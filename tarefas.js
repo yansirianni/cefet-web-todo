db05ca04d41f4b4c2b9259a5fdba254c608b7acf
Yan Victor <yanvictorsa@yahoo.com.br>
2021-07-12 07:45:27 -0300

    Load existing tasks - Exercice 1

--- tarefas.js
+++ tarefas.js
@@ -5,5 +5,24 @@ let tarefas = [{
 }, {
     nome: 'Escutar chimbinha', 
     categoria:'lazer',
-    realizada: false
+    realizada: true
 }];
+
+function insereTarefaNaPagina(tarefa){
+
+    let containerEl = document.querySelector('#lista-tarefas');
+
+    let tarefaEl = document.createElement('li');
+    
+    tarefaEl.innerHTML = `${tarefa.nome}`;
+    tarefaEl.classList.add(`item-tarefa`, `categoria-${tarefa.categoria}`)
+
+    if(tarefa.realizada){
+        tarefaEl.classList.add('marcado');
+    }
+
+    containerEl.appendChild(tarefaEl);
+}
+
+// chama a função para cada item do vetor
+tarefas.forEach(insereTarefaNaPagina);

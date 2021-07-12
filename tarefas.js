351f0720002febcee6eb8e8194231e776bc6491e
Yan Victor <yanvictorsa@yahoo.com.br>
2021-07-12 07:23:51 -0300

    Create tasks list - Exercice 0

---
+++ tarefas.js
@@ -0,0 +1,9 @@
+let tarefas = [{
+    nome: 'Comprar leite', 
+    categoria:'compras',
+    realizada: false
+}, {
+    nome: 'Escutar chimbinha', 
+    categoria:'lazer',
+    realizada: false
+}];
